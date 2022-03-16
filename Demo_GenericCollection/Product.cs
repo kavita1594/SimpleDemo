e24843ef14f170ec9fe809f1cf0c8c9de8df3746
DELL <DELL@DESKTOP-SQ6PNP6>
2022-03-16 17:59:42 +0530

    Add project files.

---
+++ Demo_GenericCollection/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_GenericCollection
+{
+    public class Product
+    {
+        private int code;
+        private String name;
+        private double price;
+
+        public Product(int code, string name, double price)
+        {
+            this.code = code;
+            this.name = name;
+            this.price = price;
+
+        }
+        public override string ToString()
+        {
+            return code + "  " + name + "  " + price;
+        }
+    }
+}
---
+++ Demo_GenericCollection/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_GenericCollection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> p = new List<Product>();
+            Product p1 = new Product(101, "abc", 1000);
+            Product p2 = new Product(102, "aaa", 2000);
+            Product p3 = new Product(103, "xyz" ,3000);
+            p.Add(p1);
+            p.Add(p2);
+            p.Add(p3);
+
+            foreach(Product i in p)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
